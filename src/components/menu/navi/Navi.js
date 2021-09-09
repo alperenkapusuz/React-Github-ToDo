2b0835cea28e3584c54b1382936e906ff3930b06
Alperen Kapusuz <63202119+alperenkapusuz@users.noreply.github.com>
2021-09-09 20:41:02 +0300

    büyük değişiklik

--- src/components/menu/navi/Navi.js
+++ src/components/menu/navi/Navi.js
@@ -4,9 +4,6 @@ import {
   Navbar,
   NavbarToggler,
   NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
 } from "reactstrap";
 
 const Navi = (props) => {
@@ -20,14 +17,6 @@ const Navi = (props) => {
         <NavbarBrand href="/dashboard">Github Todo</NavbarBrand>
         <NavbarToggler onClick={toggle} />
         <Collapse isOpen={isOpen} navbar>
-          <Nav className="mr-auto" navbar>
-            <NavItem>
-              <NavLink href="/dashboard/todo">Todo</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="/dashboard/users">Users</NavLink>
-            </NavItem>
-          </Nav>
         </Collapse>
       </Navbar>
     </div>
