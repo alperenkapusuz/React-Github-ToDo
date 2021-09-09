59aac7cde2abf46b193b323ea840568dca5394d8
Alperen Kapusuz <63202119+alperenkapusuz@users.noreply.github.com>
2021-09-09 20:50:41 +0300

    proje yeniden tasarlandı

--- src/components/menu/Dashboard.js
+++ src/components/menu/Dashboard.js
@@ -1,16 +1,9 @@
 import React from 'react'
-import {BrowserRouter, Switch, Route} from 'react-router-dom'
-import Navi from './navi/Navi'
-import Todo from './todo/Todo'
+
 
 const Dashboard = () => {
     return (
-        <BrowserRouter>
-        <Navi/>
-        <Switch>
-            <Route path="/dashboard" exact component={Todo}/>
-        </Switch>
-      </BrowserRouter>
+        <h1>to do</h1>
     )
 }
 
--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import 'antd/dist/antd.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
